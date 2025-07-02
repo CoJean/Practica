44beca921143cbe4120ea397d2ea323140323e31
ALIN-IONUT COJAN <147847769+CoJean@users.noreply.github.com>
2025-07-02 13:26:40 +0300

    Update generate_mat512.py

--- Task2/generate_mat512.py
+++ Task2/generate_mat512.py
@@ -1,6 +1,6 @@
 import random
 
 with open('mat512.in', 'w') as f:
-    for _ in range(1000):  # 1000 linii
+    for _ in range(1000):  
         matrix = [str(random.randint(0, 200)) for _ in range(25)]
         f.write(' '.join(matrix) + '\n')
