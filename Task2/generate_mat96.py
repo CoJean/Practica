b17aed09ec1683fa6d5a9006350630f06f80c6b9
ALIN-IONUT COJAN <147847769+CoJean@users.noreply.github.com>
2025-07-02 13:26:24 +0300

    Update generate_mat96.py

--- Task2/generate_mat96.py
+++ Task2/generate_mat96.py
@@ -1,6 +1,6 @@
 import random
 
 with open('mat96.in', 'w') as f:
-    for _ in range(100):  # 100 linii, poți crește numărul
+    for _ in range(100): 
         matrix = [str(random.randint(0, 100)) for _ in range(25)]
         f.write(' '.join(matrix) + '\n')
